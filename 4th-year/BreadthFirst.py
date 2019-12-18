80288b64a8dba82cad44d98bf7b4d78355b5fb39
ariG23498 <aritra.born2fly@gmail.com>
2019-12-18 11:32:18 +0530

    Obstacles are detected

--- 4th-year/BreadthFirst.py
+++ 4th-year/BreadthFirst.py
@@ -10,6 +10,13 @@ end = tuple(map(int,input("Enter the ending coordinate eg. x,y : ").split(',')))
 
 N = int(input("ENter the length of one side of the square: "))
 
+num_obstacles = int(input("Enter the number of obstacles: "))
+
+obstacles = []
+
+for i in range(num_obstacles):
+    obstacles.append(tuple(map(int,input("Enter the obstacle coordinate eg. x,y : ").split(','))))  
+
 openStates = M.MoveGen(start,N)
 closedStates = [start]
 
@@ -20,7 +27,7 @@ while(len(openStates) != 0):
         temp = M.MoveGen(openStates[0],N)
         closedStates.append(openStates[0])
         for i in temp:
-            if (not(i in openStates or i in closedStates)):
+            if (not(i in openStates or i in closedStates or i in obstacles)):
                 openStates.append(i)
         openStates.remove(openStates[0])
 print(closedStates)
